99af46517ba1d69bebf0b10e804257d9db5c001b
PushkarW <wpushkar7@gmail.com>
2024-04-04 21:34:21 +0530

    first commit with complete project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import AddBalance from "./Component/AddBalance/AddBalance";
+import AddExpences from "./Component/AddExpences/AddExpences";
+import MainPage from "./Component/MainPage/MainPage";
+import ExpencesPieChart from "./Component/Chart/ExpencesPieChart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <MainPage />
+      </div>
     </div>
   );
 }
